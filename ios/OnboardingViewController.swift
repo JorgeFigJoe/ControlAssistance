0b544404281e121fd3289420223d94e5e0d73ba8
JorgeFig <sanchezjorf@globalhitss.com>
2020-02-08 15:39:32 -0600

    [Feature CA-001]Vista Inicio sesion agregada

--- ios/OnboardingViewController.swift
+++ ios/OnboardingViewController.swift
@@ -10,21 +10,13 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var presentationImageView: UIImageView!
+  @IBOutlet weak var userTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var sesionButton: UIButton!
+  @IBOutlet weak var registerButton: UIButton!
+  override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
---
+++ ios/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  ControlAssistance
+//
+//  Created by Jorge  Figueroa on 08/02/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
