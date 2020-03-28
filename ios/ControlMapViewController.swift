0fb324d38a6f1bc512ef3bf9893f951e1fdd0f46
JorgeFig <sanchezjorf@globalhitss.com>
2020-03-28 17:57:19 -0600

    [Feature] add view to map

--- ios/ControlMapViewController.swift
+++ ios/ControlMapViewController.swift
@@ -8,19 +8,22 @@
 
 import UIKit
 import GoogleMaps
+import GooglePlaces
 
 class ControlMapViewController: UIViewController {
+  
+  var placesClient: GMSPlacesClient!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+      let camera = GMSCameraPosition.camera(withLatitude: 19.4284706, longitude: -99.1276627, zoom: 6.0)
       let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
       self.view.addSubview(mapView)
       
       let marker = GMSMarker()
-      marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-      marker.title = "Sydney"
-      marker.snippet = "Australia"
+      marker.position = CLLocationCoordinate2D(latitude: 19.4284706, longitude: -99.1276627)
+      marker.title = "CDMX"
+      marker.snippet = "Mexico"
       marker.map = mapView
     }
 
